03cd5ee880bb1a193439d8869bac67affa5a1a25
Mohammed Akram <136883858+akramlodi@users.noreply.github.com>
2025-08-08 20:42:24 +0530

    fix: linting errors

--- downloader.py
+++ downloader.py
@@ -1,7 +1,8 @@
 """
 Stock price data downloader
 Author: Mohammed Akram
-Description: This can be used to download the stock price data usign yfinance api in csv file.
+Description: This can be used to download the stock price data
+using yfinance API in CSV file.
 """
 
 import argparse
@@ -9,8 +10,9 @@ import yfinance as yf
 from datetime import datetime
 from pathlib import Path
 
+
 def download_data(ticker, start, end):
-    """Download data from yahoo finance"""
+    """Download data from Yahoo Finance."""
     try:
         data = yf.download(ticker, start=start, end=end, auto_adjust=True)
         if data.empty:
@@ -18,12 +20,12 @@ def download_data(ticker, start, end):
             return None
         return data
     except Exception as e:
-        print(f"[ERROR]: failed to download {ticker}: {e}")
+        print(f"[ERROR]: Failed to download {ticker}: {e}")
         return None
-    
+
 
 def validate_date(start, end):
-    """Validate provided date format as Year, month and day"""
+    """Validate provided date format as Year, Month, and Day."""
     try:
         datetime.strptime(start, "%Y-%m-%d")
         datetime.strptime(end, "%Y-%m-%d")
@@ -31,11 +33,11 @@ def validate_date(start, end):
     except ValueError:
         print("[ERROR] Dates must be in format YYYY-MM-DD")
         return False
-    
+
 
 def save_to_csv(ticker, data):
-    """Save the data to file"""
-    file_path = Path("data")/f"{ticker}.csv"
+    """Save the data to file."""
+    file_path = Path("data") / f"{ticker}.csv"
     data.to_csv(file_path)
     print(f"[INFO] Saved {ticker}.csv in data/")
 
@@ -47,28 +49,36 @@ def create_data_folder():
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Download stock price data from Yahoo Finance.")
-    parser.add_argument("--tickers", nargs="+", required=True, help="List of stock tickers (ex: AAPL")
-    parser.add_argument("--start", required=True, help="Start date in YYYY-MM-DD format")
-    parser.add_argument("--end", required=True, help="End date in YYYY-MM-DD format")
+    parser = argparse.ArgumentParser(
+        description="Download stock price data from Yahoo Finance."
+    )
+    parser.add_argument(
+        "--tickers", nargs="+", required=True, help="List " \
+        "of stock tickers (e.g., AAPL)"
+    )
+    parser.add_argument(
+        "--start", required=True, help="Start date in YYYY-MM-DD format"
+    )
+    parser.add_argument(
+        "--end", required=True, help="End date in YYYY-MM-DD format"
+    )
     args = parser.parse_args()
 
-    if(not(validate_date(args.start, args.end))):
+    if not validate_date(args.start, args.end):
         return None
-    
+
     create_data_folder()
-    
-    print(f"[INFO] downloading data for tickers")
+
+    print("[INFO] Downloading data for tickers.")
     for ticker in args.tickers:
         df = download_data(ticker, args.start, args.end)
         if df is not None:
             save_to_csv(ticker, df)
 
-    print("[INFO] files have been downloaded")
+    print("[INFO] Files have been downloaded.")
 
     return
 
 
 if __name__ == "__main__":
     main()
-
